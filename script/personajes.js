07e2040dfb0584b49918c1ad908216dd4b87c9ac
Fabio Isaac <fabiosaac12@gmail.com>
2022-03-21 20:56:01 -0400

    feat: personajes modall

--- script/personajes.js
+++ script/personajes.js
@@ -28,6 +28,45 @@
     nameContainer.appendChild(name);
     card.appendChild(nameContainer);
 
+    card.addEventListener("click", () => handleOpenModal(personaje));
+
     list.appendChild(card);
   }
+})();
+
+const handleOpenModal = (personaje) => {
+  const backdrop = document.getElementById("backdrop");
+
+  const image = document.getElementById("image");
+  const name = document.getElementById("name");
+  const type = document.getElementById("type");
+  const gender = document.getElementById("gender");
+  const species = document.getElementById("species");
+
+  image.setAttribute("src", personaje.image);
+  name.textContent = personaje.name;
+  type.textContent = personaje.type;
+  gender.textContent = personaje.gender;
+  species.textContent = personaje.species;
+
+  for (let property of ["gender", "species", "type"]) {
+    if (!personaje[property]) {
+      document.getElementById(property).parentNode.style.display = 'none';
+    } else {
+      document.getElementById(property).parentNode.style.display = 'flex';
+    }
+  }
+
+  backdrop.style.display = "flex";
+};
+
+const handleCloseModal = () => {
+  const backdrop = document.getElementById("backdrop");
+
+  backdrop.style.display = "none";
+};
+
+(async () => {
+  const backdrop = document.getElementById("backdrop");
+  backdrop.addEventListener("click", handleCloseModal);
 })();
